3d5b83d835dc998b13790010477626d199ca9087
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-01-20 15:47:24 +0100

    Add node-fetch dependencie and fetch HTML from URL

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const fetch = require('node-fetch');
+
+fetch('https://memegen-link-examples-upleveled.netlify.app/')
+  .then((res) => res.text())
+  .then((body) => console.log(body));
+
 // Todo: naming the folder "meme_folder"
