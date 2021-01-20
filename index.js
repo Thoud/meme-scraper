477cfd61c7a7570353670d63deb20f0b589121cb
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-01-20 16:45:31 +0100

    Extract the img links from the html

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const fetch = require('node-fetch');
 const $ = require('cheerio');
 
 // Global variables
-const imgUrlArray = [];
+const imgUrlArr = [];
 
 // Fetching HTML from URL
 fetch('https://memegen-link-examples-upleveled.netlify.app/')
   .then((res) => res.text())
-  .then((body) => console.log(body));
+  .then((body) => {
+    for (let i = 0; i < 143; i++) {
+      imgUrlArr.push($('img', body)[i].attribs.src);
+    }
+  });
 
 // Todo: naming the folder "meme_folder"
+// * 143 images
