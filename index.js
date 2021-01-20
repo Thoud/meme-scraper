1e1c827672ecbf3a017fc207cf70bf24d67884ce
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-01-20 17:14:44 +0100

    Fetch img-content from URL

--- index.js
+++ index.js
@@ -2,17 +2,22 @@
 const fetch = require('node-fetch');
 const $ = require('cheerio');
 
-// Global variables
-const imgUrlArr = [];
-
-// Fetching HTML from URL
+// Fetching HTML from URL and saving the img URLs into the array
 fetch('https://memegen-link-examples-upleveled.netlify.app/')
   .then((res) => res.text())
   .then((body) => {
+    const imgUrlArr = [];
+
     for (let i = 0; i < 143; i++) {
       imgUrlArr.push($('img', body)[i].attribs.src);
+    }
+
+    // Fetching first 10 img-contents from img URL
+    for (let i = 0; i < 10; i++) {
+      fetch(imgUrlArr[i])
+        .then((res) => res.text())
+        .then((imgContent) => console.log(imgContent));
     }
   });
 
 // Todo: naming the folder "meme_folder"
-// * 143 images
