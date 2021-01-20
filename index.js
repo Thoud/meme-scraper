9eda926ab80c435872282610448c655e07fc3136
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-01-20 16:34:33 +0100

    Add dependencies for cheerio

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+// Dependencies
 const fetch = require('node-fetch');
+const $ = require('cheerio');
 
+// Global variables
+const imgUrlArray = [];
+
+// Fetching HTML from URL
 fetch('https://memegen-link-examples-upleveled.netlify.app/')
   .then((res) => res.text())
   .then((body) => console.log(body));
